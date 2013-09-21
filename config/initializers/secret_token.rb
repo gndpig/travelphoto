08265c73aacceb7bbbaa22d2f88bb983d18407ff
Taiki Nakatsuka <gnd.scandal.pig@gmail.com>
2013-09-21 10:13:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelphoto::Application.config.secret_token = 'df9a14bce0ad96dbc103330ad8d705d184072ed9ba47eeefe43fe846f9b43d7f55e6dc903afbb12e8beacf653ff40a5a22390f0256d74c66fd9dbe2b42d92cc3'
